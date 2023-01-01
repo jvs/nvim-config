6b0d269b6c86bdb235b210b41cb84ec857bfb8ec
jvs <vonseg@protonmail.com>
2023-01-01 14:17:16 -0600

    Start using after/plugin directory.

--- init.lua
+++ init.lua
@@ -1,10 +1,5 @@
 require "system.plugins"
 
 require "user.keymaps"
-require "user.neo-tree"
 require "user.options"
-require "user.telescope"
 require "user.theme"
-require "user.toggleterm"
-require "user.treesitter"
-require "user.zen-mode"
