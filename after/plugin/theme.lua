f78b1e5151aaafa153e6229fd5e27d09f505ccfc
jvs <vonseg@protonmail.com>
2023-01-01 15:05:07 -0600

    Keep moving things around.

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,3 +1,11 @@
+local colorscheme = 'vscode'
+
+local status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
+if not status_ok then
+  vim.notify('colorscheme ' .. colorscheme .. ' not found!')
+  return
+end
+
 
 -- Highlight on yank.
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
--- init.lua
+++ init.lua
@@ -2,4 +2,3 @@ require "user.packer"
 
 require "user.keymaps"
 require "user.options"
-require "user.theme"
