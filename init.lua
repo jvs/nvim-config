80e8727ad847a8e417694ced9407bb88d3af3d46
jvs <vonseg@protonmail.com>
2023-09-05 10:00:13 -0500

    Add local plugins.

--- init.lua
+++ init.lua
@@ -2,6 +2,14 @@
 -- install and update nvim plugins.
 -- require "user.packer"
 
+-- Temporary:
+vim.opt.runtimepath:append("~/github/jvs/nvim-plugins/commanderly.nvim")
+vim.opt.runtimepath:append("~/github/jvs/nvim-plugins/split-personality.nvim")
+vim.opt.runtimepath:append("~/github/jvs/nvim-plugins/symbolism.nvim")
+require('commanderly').setup()
+require('split-personality').setup()
+require('symbolism').setup()
+
 require "user.commands"
 require "user.keymaps"
 require "user.options"
