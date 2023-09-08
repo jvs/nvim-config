586c9d2ec93217306101abafdf511749cc4edf7f
jvs <vonseg@protonmail.com>
2023-09-07 23:40:11 -0500

    Hide the winbar (for real this time).

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,3 +1,9 @@
+local has_lualine, lualine = pcall(require, "lualine")
+
+if has_lualine then
+  lualine.hide({place = {'winbar'}})
+end
+
 vim.wo.spell = true
 vim.opt_local.winbar = nil
 vim.api.nvim_win_set_cursor(0, {1, 0})
