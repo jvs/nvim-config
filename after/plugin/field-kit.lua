6c0ee902ab4c65677332fa9eb1958b949ffb074b
jvs <vonseg@protonmail.com>
2026-04-02 19:43:13 -0400

    Start adding field-kit rules.

---
+++ after/plugin/field-kit.lua
@@ -0,0 +1,16 @@
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local cwd = vim.fn.getcwd()
+
+    if cwd:match("/kit/todo") then
+      vim.o.colorcolumn = ""
+      vim.cmd("colorscheme mellifluous")
+
+    elseif cwd:match("/kit/junk") then
+      vim.cmd("colorscheme melange")
+
+    elseif cwd:match("/kit/notes") then
+      vim.cmd("colorscheme nord")
+    end
+  end,
+})
