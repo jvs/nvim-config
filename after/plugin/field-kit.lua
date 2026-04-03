3fcb4d13e4f24dba7bfa33f71b2e6021c8371253
jvs <vonseg@protonmail.com>
2026-04-03 07:24:48 -0400

    Change colorscheme at the right time.

--- after/plugin/field-kit.lua
+++ after/plugin/field-kit.lua
@@ -1,16 +1,18 @@
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
-    local cwd = vim.fn.getcwd()
+    vim.schedule(function()
+      local cwd = vim.fn.getcwd()
 
-    if cwd:match("/kit/todo") then
-      vim.o.colorcolumn = ""
-      vim.cmd("colorscheme mellifluous")
+      if cwd:match("/kit/todo") then
+        vim.o.colorcolumn = ""
+        vim.cmd("colorscheme mellifluous")
 
-    elseif cwd:match("/kit/junk") then
-      vim.cmd("colorscheme melange")
+      elseif cwd:match("/kit/junk") then
+        vim.cmd("colorscheme melange")
 
-    elseif cwd:match("/kit/notes") then
-      vim.cmd("colorscheme nord")
-    end
+      elseif cwd:match("/kit/notes") then
+        vim.cmd("colorscheme nord")
+      end
+    end)
   end,
 })
