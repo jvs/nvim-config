00ba84bd8d22024b00588faf5c445891fb864ed7
jvs <vonseg@gmail.com>
2026-04-04 23:35:31 -0500

    Improve commit message and pi-prompt configurations.

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,11 +1,12 @@
-local has_lualine, lualine = pcall(require, "lualine")
+local has_commanderly, commanderly = pcall(require, "commanderly")
 
-if has_lualine then
-  lualine.hide({place = {'winbar'}})
+if has_commanderly then
+  commanderly.run("enable_spell_check")
+  commanderly.run("hide_winbar")
+  -- commanderly.run("show_simple_statusline")
 end
 
-vim.wo.spell = true
-vim.opt_local.winbar = nil
+vim.cmd("colorscheme vscode")
 vim.api.nvim_win_set_cursor(0, {1, 0})
 
 if vim.fn.getline(1) == '' then
---
+++ after/plugin/pi-md.lua
@@ -0,0 +1,25 @@
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    vim.schedule(function()
+      local full_path = vim.api.nvim_buf_get_name(0)
+
+      if full_path:find("/var/") and full_path:match("%.pi%.md$") then
+        vim.o.colorcolumn = ""
+        vim.cmd("colorscheme tokyonight-storm")
+
+        local has_commanderly, commanderly = pcall(require, "commanderly")
+
+        if has_commanderly then
+          commanderly.run("enable_word_wrap")
+          commanderly.run("enable_spell_check")
+          commanderly.run("hide_winbar")
+          commanderly.run("show_simple_statusline")
+        end
+
+        if not has_commanderly and not vim.o.wrap then
+          vim.o.wrap = true
+        end
+      end
+    end)
+  end,
+})
